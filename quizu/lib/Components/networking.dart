ff78c577e844ae7c99e6f4f1e945d82ff42b7c22
Jaafar Al-Muallim <Ja3far03@gmail.com>
2022-09-26 11:13:33 +0300

    Updated Networking not to be hard coded

--- quizu/lib/Components/networking.dart
+++ quizu/lib/Components/networking.dart
@@ -25,6 +25,7 @@ class NetworkingHelper {
   Future<dynamic> login(String otp, String mobile) async {
     http.Response res = await http
         .post(Uri.parse(loginUrl), body: {'OTP': otp, 'mobile': '0$mobile'});
+    // .post(Uri.parse(loginUrl), body: {'OTP': '0000', 'mobile': '09'});
     if (res.statusCode >= 200 || res.statusCode < 400) {
       dynamic data = await jsonDecode(res.body);
       myToken = data['token'];
